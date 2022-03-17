7becc33f56534609627e8ce3818096555902ebd8
June Choe <jchoe001@gmail.com>
2022-03-17 11:40:28 -0400

    add option to expand contractions

--- R/1-helpers-text.R
+++ R/1-helpers-text.R
@@ -3,11 +3,12 @@ contractions <- lapply(lexicon::key_contractions, tolower)
 #' Clean up text
 #'
 #' @param string Length-1 character
+#' @param expand_contractions Whether to expand contractions. Defaults to `TRUE`
 #'
 #' @return Character
 #' @export
 #'
-clean_text <- function(string) {
+clean_text <- function(string, expand_contractions = TRUE) {
   trimmed <- stringi::stri_replace_all_regex(stringi::stri_replace_all_regex(string, "(^\\s+|\\s+$)", ""), "\\s+", " ")
   sentences <- stringi::stri_extract_all_regex(trimmed, "[^.?!]+")[[1]]
   sentences_trimmed <- stringi::stri_replace_all_regex(sentences, "(^+\\s|\\s+$)", "")
@@ -16,18 +17,23 @@ clean_text <- function(string) {
   sentence_words <- lapply(squished, function(x) {
     lowercase <- stringi::stri_trans_tolower(x)
     lowercase_split <- stringi::stri_split_regex(lowercase, "(\\s|-)")[[1]]
-    contractions_substituted <- lapply(lowercase_split, function(y) {
-      m <- match(y, contractions$contraction)
-      if (!is.na(m)) {
-        contractions$expanded[m]
-      } else {
-        y
-      }
-    })
-    stringi::stri_split_fixed(stringi::stri_flatten(contractions_substituted, collapse = " "), " ")[[1]]
+    words <- if (expand_contractions) {
+      lapply(lowercase_split, function(y) {
+        m <- match(y, contractions$contraction)
+        if (!is.na(m)) {
+          contractions$expanded[m]
+        } else {
+          y
+        }
+      })
+    } else {
+      unlist(lowercase_split, use.names = FALSE)
+    }
+    stringi::stri_split_fixed(stringi::stri_flatten(words, collapse = " "), " ")[[1]]
   })
   sentence_words <- unlist(sentence_words, use.names = FALSE, recursive = FALSE)
-  stringi::stri_replace_all_regex(sentence_words, "[[:punct:]]", "")
+  ifelse(sentence_words %in% contractions$contraction, sentence_words,
+         stringi::stri_replace_all_regex(sentence_words, "[[:punct:]]", ""))
 }
 
 # Dictionaries ----
